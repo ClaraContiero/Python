f947b8dc81e92f1b9e1f5587f0af3ba85e3a440f
Clara Contiero <contiero.clara6@gmail.com>
2024-02-06 21:47:20 -0300

    Alterando...

--- Desafios/DES_008.py
+++ Desafios/DES_008.py
@@ -1,4 +1,4 @@
 monei = float(input('Digite, em R$, a quantidade de dinheiro que você possui: \n'))
 convert = monei/3.27
 
-print(f'Você pode comprar {convert:.2f} dólares.')
+print(f'Você pode comprar aaaaaaaaa {convert:.2f} dólares.')
