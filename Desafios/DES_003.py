1f135acb67f9decde1835b90f6914fc918ae8985
Clara Contiero <contiero.clara6@gmail.com>
2024-02-06 21:33:35 -0300

    Alterando...

--- Desafios/DES_003.py
+++ Desafios/DES_003.py
@@ -2,4 +2,6 @@ num = int(input('Digite um número: \n'))
 print(f'Número: {num}\n'
       f'Dobro: {num*2}\n'
       f'Triplo: {num*3}\n'
-      f'Raiz: {num**(1/2)}')
+      f'Raiz: {num**(1/2):.1f}')
+
+# potência pow(base, expoente)
